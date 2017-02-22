6563bf5eb56ec659a9e7a3540e4742cd94c0c6ef
Aleksandra <o.barecka@gmail.com>
2017-02-22 21:18:49 +0100

    modal gallery done; to be checked on mobile

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-console.log("works");
-
-// ----------------- sticky nav -----------------
+'use strict';
 $(document).ready(function(){
-      $(window).scroll(function() { 
-        if ($(document).scrollTop() > 50) {
-          $("nav").css("background-color", "rgba(0,0,0,.9)");
-        } else {
-          $("nav").css("background-color", "transparent");
-        }
-      });
-    });
+// ----------------- sticky nav -----------------
+$(window).scroll(function() { 
+  if ($(document).scrollTop() > 50) {
+    $("nav").css("background-color", "rgba(0,0,0,.93)");
+  } else {
+    $("nav").css("background-color", "transparent");
+  }
+});
+
 
 
 // ----------------- smooth scrolling -----------------
@@ -42,3 +41,44 @@ $(".burgerIcon").click(function(){
   $("nav").toggleClass("mobile");
 });
 
+
+// -----------------  modal gallery  -----------------
+// images must be named "number.format" i.e 1.jpg or 4.png; place images in subfolder "gallery" in "img" folder or change code
+//*close modal wind*
+$(".closeModalBttn").click(function(){  
+  $(".modalGallery").css({"display":"none"});
+});
+
+//*open modal wind*
+$("#portfolio img").click(function(){
+  var $clickedImgSrc = $(this).attr('src'), $srcForModalPic = "url('" + $clickedImgSrc + "')";
+  $(".modalGallery").css({"display":"block"});
+  $(".galleryPicture").css({"background-image": $srcForModalPic});
+});
+
+//*scroll picture*
+$(".scrollPic").click(function(){
+  var $mathSign = "", 
+  $currentImgUrl = $(".galleryPicture").css('background-image'),
+  $currentImgNum = $currentImgUrl.slice(-7).substring(0,1),
+  $nextImgNum = 0,
+  $maxNumOfItemsInGalleryFolder = 9,  //manually declared;
+  $srcForModalPic=''; 
+
+      this.id == "prevPicBttn" ? $mathSign = "-" : $mathSign = "+";
+      $nextImgNum = eval($currentImgNum + $mathSign + 1);
+      if ($nextImgNum > $maxNumOfItemsInGalleryFolder) {
+        $nextImgNum = 1;
+      }
+      else if ($nextImgNum < 1) {
+        $nextImgNum = $maxNumOfItemsInGalleryFolder;
+      }
+      $srcForModalPic = "url('../img/gallery/" + $nextImgNum + ".jpg')";
+      $(".galleryPicture").css({"background-image": $srcForModalPic});
+    });
+
+
+
+
+//the end
+});
